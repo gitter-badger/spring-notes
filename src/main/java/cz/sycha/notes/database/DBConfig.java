09a4056d7f1677f39333f7d7e6e01e76497dab3a
Jakub Sycha <jacob@programujem.cz>
2016-01-18 11:09:02 +0100

    Added comments to config, (YouTrack sux), #SN-1 Fixed

--- src/main/java/cz/sycha/notes/database/DBConfig.java
+++ src/main/java/cz/sycha/notes/database/DBConfig.java
@@ -1,5 +1,10 @@
 package cz.sycha.notes.database;
 
+/**
+ * Database configuration class
+ * @license MIT
+ * @author Jakub Sycha
+ */
 public class DBConfig {
     // Keep this uncommented if you want to load settings from environment variables
     private static final String DB_HOST = System.getenv("SPRING_NOTE_DB_HOST");
@@ -20,26 +25,56 @@ public class DBConfig {
         private static final String DB_PASS = null;
      */
 
+    /**
+     * Returns the database host
+     *
+     * @return String DB_HOST
+     */
     public static String getDbHost() {
         return DB_HOST;
     }
 
+    /**
+     * Returns database port
+     *
+     * @return int DB_PORT
+     */
     public static int getDbPort() {
         return DB_PORT;
     }
 
+    /**
+     * Returns Database User
+     *
+     * @return String DB_USER
+     */
     public static String getDbUser() {
         return DB_USER;
     }
 
+    /**
+     * Returns Database password
+     *
+     * @return String DB_PASS
+     */
     public static String getDbPass() {
         return DB_PASS;
     }
 
+    /**
+     * returns Database Name
+     *
+     * @return String DB-NAME
+     */
     public static String getDbName() {
         return DB_NAME;
     }
 
+    /**
+     * Returns DB collection
+     *
+     * @return String DB_COLLECTION
+     */
     public static String getDbCollection() {
         return DB_COLLECTION;
     }
